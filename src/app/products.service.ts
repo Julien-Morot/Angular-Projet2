29621c17b3fca495c9bcf8123247811be38a7e9d
Julien Morot <julien.morot@devinci.fr>
2019-09-19 11:57:23 +0200

    Etape 16

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -9,19 +9,38 @@ export class ProductsService {
   uri = 'http://localhost:4000/products';
 
   constructor(private http: HttpClient) { }
-  getProducts() {
-    return this
-           .http
-           .get(`${this.uri}`);
-  }
+
   addProduct(ProductName, ProductDescription, ProductPrice) {
+    console.log(ProductName, ProductDescription, ProductPrice);
     const obj = {
       ProductName,
       ProductDescription,
       ProductPrice
     };
-    console.log(obj);
     this.http.post(`${this.uri}/add`, obj)
+        .subscribe(res => console.log('Done'));
+  }
+
+  getProducts() {
+    return this
+           .http
+           .get(`${this.uri}`);
+  }
+
+  editProduct(id) {
+    return this
+            .http
+            .get(`${this.uri}/edit/${id}`);
+    }
+    updateProduct(ProductName, ProductDescription, ProductPrice, id) {
+      const obj = {
+        ProductName,
+        ProductDescription,
+        ProductPrice
+      };
+      this
+        .http
+        .post(`${this.uri}/update/${id}`, obj)
         .subscribe(res => console.log('Done'));
   }
 }
